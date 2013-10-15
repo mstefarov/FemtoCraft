c7e5e6a57baa298c711d260f1afa73c33a197228
fragmer <fragmer@0b873793-0d68-4481-90a4-b2c271ba64ab>
2013-10-15 20:17:56 +0000

    Updated LineWrapper, now based on fCraft 0.900_r2232_dev

--- FemtoCraft/Networking/LineWrapper.cs
+++ FemtoCraft/Networking/LineWrapper.cs
@@ -1,5 +1,5 @@
-﻿// Part of FemtoCraft | Copyright 2012-2013 Matvei Stefarov <me@matvei.org> | See LICENSE.txt
-// Based on fCraft.LineWrapper - fCraft is Copyright 2009-2012 Matvei Stefarov <me@matvei.org> | See LICENSE.fCraft.txt
+﻿// Part of fCraft | Copyright 2009-2013 Matvei Stefarov <me@matvei.org> | BSD-3 | See LICENSE.txt
+// #define DEBUG_LINE_WRAPPER
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,34 +11,39 @@ namespace FemtoCraft {
         const string DefaultPrefixString = "> ";
         static readonly byte[] DefaultPrefix;
 
+
         static LineWrapper() {
             DefaultPrefix = Encoding.ASCII.GetBytes( DefaultPrefixString );
         }
 
-        const int PacketSize = 66; // opcode + id + 64
-        const byte NoColor = (byte)'f';
+
+        const int PacketSize = 66; // opCode + id + 64
+        const byte DefaultColor = (byte)'f';
 
         public Packet Current { get; private set; }
 
-        byte color,
-             lastColor;
-        bool hadColor, // used to see if white (&f) colorcodes should be inserted
-             hadSpace; // used to see if a word needs to be forcefully wrapped (i.e. doesnt fit in one line)
-        int spaceCount,
-            wordLength; // used to see whether to wrap at hyphens
+        bool expectingColor;// whether next input character is expected to be a color code
+        byte color,         // color that the next inserted character should be
+             lastColor;     // used to detect duplicate color codes
+
+        bool hadColor,      // used to see if white (&f) color codes should be inserted
+             canWrap;       // used to see if a word needs to be forcefully wrapped (i.e. doesn't fit in one line)
+
+        int spaceCount,     // used to track spacing between words
+            wordLength;     // used to see whether to wrap at hyphens
+
+        readonly byte[] prefix;
 
         readonly byte[] input;
         int inputIndex;
 
         byte[] output;
-        int outputStart, outputIndex;
+        int outputStart,
+            outputIndex;
 
-        readonly byte[] prefix;
-
-        int wrapIndex,
-            wrapOutputIndex;
-        byte wrapColor;
-        bool expectingColor;
+        int wrapInputIndex,     // index of the nearest line-wrapping opportunity in the input buffer 
+            wrapOutputIndex;    // corresponding index in the output buffer
+        byte wrapColor;         // value of "color" field at the wrapping point
 
 
         public LineWrapper( [NotNull] string message ) {
@@ -50,10 +55,10 @@ namespace FemtoCraft {
 
 
         public void Reset() {
-            color = NoColor;
+            color = DefaultColor;
             wordLength = 0;
             inputIndex = 0;
-            wrapIndex = 0;
+            wrapInputIndex = 0;
             wrapOutputIndex = 0;
         }
 
@@ -62,33 +67,36 @@ namespace FemtoCraft {
             if( inputIndex >= input.Length ) {
                 return false;
             }
-            hadColor = false;
 
             output = new byte[PacketSize];
             output[0] = (byte)OpCode.Message;
+            Current = new Packet( output );
+
+            hadColor = false;
+            canWrap = false;
+            expectingColor = false;
 
             outputStart = 2;
             outputIndex = outputStart;
             spaceCount = 0;
-            lastColor = NoColor;
-            Current = new Packet( output );
 
-            wrapColor = NoColor;
-            expectingColor = false;
+            lastColor = DefaultColor;
+
             wrapOutputIndex = outputStart;
+            wrapColor = color;
 
             // Prepend line prefix, if needed
             if( inputIndex > 0 && prefix.Length > 0 ) {
                 int preBufferInputIndex = inputIndex;
                 byte preBufferColor = color;
-                color = NoColor;
+                color = DefaultColor;
                 inputIndex = 0;
-                wrapIndex = 0;
+                wrapInputIndex = 0;
                 wordLength = 0;
                 while( inputIndex < prefix.Length ) {
                     byte ch = prefix[inputIndex];
                     if( ProcessChar( ch ) ) {
-                        // Should never happen, since prefix is under 32 chars
+                        // Should never happen, since prefix is under 48 chars
                         throw new Exception( "Prefix required wrapping." );
                     }
                     inputIndex++;
@@ -99,8 +107,8 @@ namespace FemtoCraft {
             }
 
             wordLength = 0;
-            wrapIndex = inputIndex;
-            hadSpace = false;
+            wrapInputIndex = inputIndex;
+            canWrap = false; // to prevent line-wrapping at prefix
 
             // Append as much of the remaining input as possible
             while( inputIndex < input.Length ) {
@@ -113,6 +121,7 @@ namespace FemtoCraft {
                 inputIndex++;
             }
 
+            // No more input (last line)
             PrepareOutput();
             return true;
         }
@@ -121,11 +130,11 @@ namespace FemtoCraft {
         bool ProcessChar( byte ch ) {
             switch( ch ) {
                 case (byte)' ':
-                    hadSpace = true;
+                    canWrap = true;
                     expectingColor = false;
                     if( spaceCount == 0 ) {
                         // first space after a word, set wrapping point
-                        wrapIndex = inputIndex;
+                        wrapInputIndex = inputIndex;
                         wrapOutputIndex = outputIndex;
                         wrapColor = color;
                     }
@@ -133,32 +142,34 @@ namespace FemtoCraft {
                     break;
 
                 case (byte)'&':
+                    // skip double ampersands
                     expectingColor = !expectingColor;
                     break;
 
                 case (byte)'-':
                     if( spaceCount > 0 ) {
                         // set wrapping point, if at beginning of a word
-                        wrapIndex = inputIndex;
+                        wrapInputIndex = inputIndex;
                         wrapColor = color;
                     }
                     expectingColor = false;
                     if( !Append( ch ) ) {
-                        if( hadSpace ) {
+                        if( canWrap ) {
                             // word doesn't fit in line, backtrack to wrapping point
-                            inputIndex = wrapIndex;
+                            inputIndex = wrapInputIndex;
                             outputIndex = wrapOutputIndex;
                             color = wrapColor;
-                        }// else force wrap (word is too long), don't backtrack
+                        } // else force wrap (word is too long), don't backtrack
                         return true;
                     }
                     spaceCount = 0;
                     if( wordLength > 2 ) {
                         // allow wrapping after hyphen, if at least 2 word characters precede this hyphen
-                        wrapIndex = inputIndex + 1;
+                        wrapInputIndex = inputIndex + 1;
                         wrapOutputIndex = outputIndex;
                         wrapColor = color;
                         wordLength = 0;
+                        canWrap = true;
                     }
                     break;
 
@@ -171,25 +182,26 @@ namespace FemtoCraft {
                     if( expectingColor ) {
                         expectingColor = false;
                         if( ProcessColor( ref ch ) ) {
+                            // valid color code
                             color = ch;
                             hadColor = true;
-                        }// else colorcode is invalid, skip
+                        } // else color code is invalid, skip
                     } else {
                         if( spaceCount > 0 ) {
                             // set wrapping point, if at beginning of a word
-                            wrapIndex = inputIndex;
+                            wrapInputIndex = inputIndex;
                             wrapColor = color;
                         }
-                        if( !IsWordChar( ch ) ) {
+                        if( ch == 0 || ch > 127 ) {
                             // replace unprintable chars with '?'
                             ch = (byte)'?';
                         }
                         if( !Append( ch ) ) {
-                            if( hadSpace ) {
-                                inputIndex = wrapIndex;
+                            if( canWrap ) {
+                                inputIndex = wrapInputIndex;
                                 outputIndex = wrapOutputIndex;
                                 color = wrapColor;
-                            }// else word is too long, dont backtrack to wrap
+                            } // else word is too long, don't backtrack to wrap
                             return true;
                         }
                     }
@@ -208,14 +220,16 @@ namespace FemtoCraft {
 
 
         bool Append( byte ch ) {
+            bool prependColor =
+                // color changed since last inserted character, OR
+                lastColor != color ||
+                // a color code is needed to preserve leading whitespace
+                (color == DefaultColor && hadColor && outputIndex == outputStart && spaceCount > 0);
+
             // calculate the number of characters to insert
-            int bytesToInsert = 1;
-            if( ch == (byte)'&' ) bytesToInsert++;
-
-            bool prependColor = ( lastColor != color || ( color == NoColor && hadColor && outputIndex == outputStart ) );
-
+            int bytesToInsert = 1 + spaceCount;
             if( prependColor ) bytesToInsert += 2;
-            if( outputIndex + bytesToInsert + spaceCount > PacketSize ) {
+            if( outputIndex + bytesToInsert > PacketSize ) {
                 return false;
             }
 
@@ -237,19 +251,11 @@ namespace FemtoCraft {
             wordLength += bytesToInsert;
 
             // append character
-            if( ch == (byte)'&' ) output[outputIndex++] = ch;
             output[outputIndex++] = ch;
             return true;
         }
 
 
-        [Pure]
-        static bool IsWordChar( byte ch ) {
-            return ( ch > (byte)' ' && ch <= (byte)'~' );
-        }
-
-
-        [Pure]
         static bool ProcessColor( ref byte ch ) {
             if( ch >= (byte)'A' && ch <= (byte)'Z' ) {
                 ch += 32;
@@ -265,14 +271,20 @@ namespace FemtoCraft {
         }
 
 
+        void IDisposable.Dispose() { }
+
+
+        #region IEnumerable<Packet> Members
+
         public IEnumerator<Packet> GetEnumerator() {
             return this;
         }
+
 
         IEnumerator IEnumerable.GetEnumerator() {
             return this;
         }
 
-        void IDisposable.Dispose() { }
+        #endregion
     }
 }
